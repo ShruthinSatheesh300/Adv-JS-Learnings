ab171b5b3a0d5fb08909658d71a54711abe94713
Shruthin Satheesh <95200511+ShruthinSatheesh300@users.noreply.github.com>
2022-02-18 22:47:45 +0530

    Added ES-7 Features learning

---
+++ ES 7 Features/ES-7.js
@@ -0,0 +1,26 @@
+// 1 .Exponentiation Operator
+// The exponentiation operator (**) raises the first operand to the power of the second operand.
+
+// Example
+let x = 5;
+let z = x ** 2;          // result is 25
+
+//x ** y produces the same result as Math.pow(x, y):
+//...................................................
+
+// 2. Exponentiation Assignment
+// The exponentiation assignment operator (**=) raises the value of 
+//a variable to the power of the right operand.
+// Example
+let x = 5;
+x **= 2; // result 25
+//.....................................................
+
+// 3. JavaScript Array.includes()
+// ES-7 introduced Array.prototype.includes to arrays. This allows us 
+//to check if an element is present in an array:
+
+// Example
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.includes("Mango"); // is true
